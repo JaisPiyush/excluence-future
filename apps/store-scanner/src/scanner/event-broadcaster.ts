e15540a40cc8d34d7e34a95dfe3f2d0321be492b
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-07-14 17:12:52 +0530

    Func to handle multiple events in single block

--- apps/store-scanner/src/scanner/event-broadcaster.ts
+++ apps/store-scanner/src/scanner/event-broadcaster.ts
@@ -1,16 +1,18 @@
 import { EventBroadcasterInterface } from "@rayvin-flow/flow-scanner-lib/lib/broadcaster/event-broadcaster";
 import { FlowEvent } from "@rayvin-flow/flow-scanner-lib/lib/flow/models/flow-event";
 import { defaultQueue } from "steward";
-import { AddListingJob } from "steward/src/jobs/AddListing.job";
 import { getEventJobHandler } from "./events";
 
 export class QueueEventBroadcaster implements EventBroadcasterInterface {
     broadcastEvents =  async (blockHeight: number, events: FlowEvent[]) : Promise<void> => {
-        const name = Date.now().toString();
-        const jobClass = getEventJobHandler(events[0].type);
-        if (jobClass) {
-            await defaultQueue.add(name, new jobClass(events[0]));
+
+        for (const event of events) {
+            const name = Date.now().toString();
+            const jobClass = getEventJobHandler(event.type);
+            if (jobClass) {
+                await defaultQueue.add(name, new jobClass(event));
+            }
         }
-        
+                       
     }
 }
